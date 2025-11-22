efdbda59c0ab8b702b0c3f833998cd88b5e70b94
aliffirmansyah14 <aliffirmansyah1407@gmail.com>
2025-11-22 14:00:37 +0700

    init/prisma add comment inline

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  // biar jadi http://addess/api/*
   app.setGlobalPrefix('api');
   // class validator
   app.useGlobalPipes(
